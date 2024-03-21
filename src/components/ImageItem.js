e9021758080da5dedffe8ad42bbbed29c620128c
Dursun Köktürk <dursunserefkokturk@gmail.com>
2024-03-21 14:21:54 +0300

    request-api-using-axios-and-use-the-returned-response

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -1,16 +1,10 @@
-// Api Icindeki Image lerin Bulundugu 
-// Degisken Adini Parametre Olarak Veriyoruz
 function ImageItem({ image }) {
   console.log(image);
   return (
     <div>
       <img
         className="imageListImg"
-
-        // Image Bilgilerinin Small Halini Aliyoruz
         src={image.urls.small}
-
-        // Image Bilgilerinin Aciklamalarini Aliyoruz
         alt={image.alt_description}
       />
     </div>
